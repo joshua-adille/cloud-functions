f77d14ea8e8c2812b9d246865785699237f6b787
Joshua <joshua.adille14@gmail.com>
2023-11-08 08:37:31 +0800

    added main component add note function and its css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,53 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Firestore, collectionData, collection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, map } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
-interface Item {
-  description: string;
-}
+import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  item$: Observable<any>;
-  firestore: Firestore = inject(Firestore);
+  constructor(private service: SharedService) {}
+  notes: any = [];
+  category: any = [];
 
-  constructor() {
-    const itemCollection = collection(this.firestore, 'items');
-    this.item$ = collectionData(itemCollection);
+  getData() {
+    const notes$ = this.service.getNotes();
+    const categories$ = this.service.getCategory();
+    notes$
+      .pipe(
+        switchMap((notes) => {
+          return categories$.pipe(
+            map((categories) => {
+              return { notes: notes, categories: categories };
+            })
+          );
+        })
+      )
+      .subscribe((res) => {
+        this.notes = res.notes;
+        this.category = res.categories;
+      });
+  }
+  ngOnInit() {
+    this.getData();
+  }
+  addNotes(newNotes: string) {
+    this.service
+      .addNote(newNotes)
+      .then((res) => {
+        console.log(res);
+        this.getData();
+      })
+      .catch((error) => {
+        console.log(`There was an error! ${error}`);
+      });
   }
 }
