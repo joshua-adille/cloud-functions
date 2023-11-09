d8ce24a6add81410b13825efdc9522009b251932
Joshua <joshua.adille14@gmail.com>
2023-11-09 14:30:17 +0800

    changed the app.config.ts to be able to communicate with the local firestore emulator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,41 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { firebaseConfig } from '../firebase.config';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  getFirestore,
+  provideFirestore,
+  connectFirestoreEmulator,
+  initializeFirestore,
+  Firestore,
+} from '@angular/fire/firestore';
+import {
+  getFunctions,
+  provideFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom([
       provideFirebaseApp(() => initializeApp(firebaseConfig)),
-      provideFirestore(() => getFirestore()),
+      // provideFirestore(() => getFirestore()),
+      provideFirestore(() => {
+        let firestore: Firestore;
+        firestore = initializeFirestore(getApp(), {
+          experimentalForceLongPolling: true,
+        });
+        connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+        return firestore;
+      }),
       provideFunctions(() => getFunctions()),
+      provideFunctions(() => {
+        let functions = getFunctions(getApp());
+        connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+
+        return functions;
+      }),
     ]),
   ],
 };
